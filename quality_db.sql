2e3debc5dcecbd3a5e4d99e1687e00f2528f5d9a
Kelly Kwon <ksy.kwon@gmail.com>
2019-05-13 21:16:10 -0700

    mysql updated

--- quality_db.sql
+++ quality_db.sql
@@ -3,18 +3,18 @@ USE Quality_db;
 
 -- Create tables for raw data to be loaded into
 drop table IF EXISTS quality_final;
-CREATE TABLE quality_final (
+CREATE TABLE IF NOT EXISTS quality_final (
   Rank INT PRIMARY KEY,
   City TEXT,
-  QOL_Index TEXT,
-  PP_Index TEXT,
-  Safety_Index TEXT,
-  Health_Index TEXT,
-  CL_Index TEXT,
-  PPIR_Index TEXT,
-  TCT_Index TEXT,
-  Pollution_Index TEXT,
-  Climate_Index TEXT,
+  QOL_Index DECIMAL(5,2),
+  PP_Index DECIMAL(5,2),
+  Safety_Index DECIMAL(5,2),
+  Health_Index DECIMAL(5,2),
+  CL_Index DECIMAL(5,2),
+  PPIR_Index DECIMAL(5,2),
+  TCT_Index DECIMAL(5,2),
+  Pollution_Index DECIMAL(5,2),
+  Climate_Index DECIMAL(5,2),
   Country TEXT,
   Metro TEXT,
   State TEXT,
@@ -22,9 +22,7 @@ CREATE TABLE quality_final (
   Abbreviation TEXT
 );
 
-
-
-
+-- check table;
 select * from quality_final;
 
 
